be4a691080f3cb1246b1781e051ed5b831a0290a
eman275 <eman.altweel11@gmail.com>
2020-11-20 14:42:43 +0200

    swiper lib

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+      var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 5.2,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+
+   
+
+    document.addEventListener('contextmenu' , event => event.preventDefault());
